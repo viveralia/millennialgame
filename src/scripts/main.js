032645900cef45f2e77f88000e5c1cf88d1f3b2d
viveralia <edgarv10@gmail.com>
2019-08-30 11:40:20 -0500

    Woohoo 🙌

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,19 +1,34 @@
 /********************************************/
 /**************** VARIABLES ****************/
 /******************************************/
+const canvasWrapper = document.querySelector('.canvas-wrapper')
 const canvas = document.querySelector('canvas')
 const ctx = canvas.getContext('2d')
-// Welcome Screen
+// Screens
 const welcomeScreen = document.querySelector('.welcome')
+const gameOverScreen = document.querySelector('.game-over')
 // Obstacles
 const obstacles = []
-// Counters
-let frames = 0
-let score = 0
 // Interval
 let interval
+// Counters
+let frames = 0
+// Score Board
+let score = 0
+const scoreBoard = document.querySelector('.score')
+// Others
+let gravity = 0.98
+let keys = {}
 // Audios
+const bgSong = document.querySelector('#bg-song')
 const gameOverSong = document.querySelector('#game-over-song')
+const jumpSound = document.querySelector('#jump-sound')
+// Sharer buttons & final points
+const finalScore = document.querySelector('#score-span')
+const whatsSharer = document.querySelector('#whats-share')
+const fbSharer = document.querySelector('#fb-share')
+const mailSharer = document.querySelector('#mail-share')
+const restartButton = document.querySelector('#restart')
 
 /***********************************************/
 /******************* CLASSES ******************/
@@ -25,19 +40,17 @@ class Runner {
     this.height = height
     this.x = 40
     this.y = canvas.height - this.height
-    // Jumping values
-    this.maxJumpHeight = (canvas.height - this.height) / 2 - 200
-    this.floor = canvas.height - this.height
     // Creates a new img instance with the Image() class and adds the src attribute
     this.img = new Image()
     this.img.src = 'https://i1.wp.com/espacioenblan.co/wp-content/uploads/2019/08/girl.png'
+    // Jumping
+    this.velY = 0
+    this.grounded = false
+    this.jumping = false
+    this.jumpStrength = 11
   }
   draw() {
     ctx.drawImage(this.img, this.x, this.y, this.width, this.height)
-    if (this.y >= this.maxJumpHeight && this.y < this.floor) this.y += 20
-  }
-  jump() {
-    this.y >= this.floor && (this.y = this.maxJumpHeight)
   }
   crashedWith(obstacle) {
     return (
@@ -60,13 +73,25 @@ class Obstacle {
     // Obstacle 0: Taxes
     this.imgTaxes = new Image()
     this.imgTaxes.src = 'https://i1.wp.com/espacioenblan.co/wp-content/uploads/2019/08/taxes.png'
+    // Obstacle 1: Taxes
+    this.imgWedding = new Image()
+    this.imgWedding.src = 'https://i1.wp.com/espacioenblan.co/wp-content/uploads/2019/08/conv.png'
+    // Obstacle 2: Taxes
+    this.imgHangover = new Image()
+    this.imgHangover.src = 'https://i1.wp.com/espacioenblan.co/wp-content/uploads/2019/08/cruda.png'
   }
   draw() {
     // Keeps moving the obstacle to the left
-    this.x -= 15
+    score <= 40 ? (this.x -= 10) : (this.x -= 13)
     switch (this.type) {
       case 0:
         ctx.drawImage(this.imgTaxes, this.x, this.y, this.width, this.height)
+        break
+      case 1:
+        ctx.drawImage(this.imgWedding, this.x, this.y, this.width, this.height)
+        break
+      case 2:
+        ctx.drawImage(this.imgHangover, this.x, this.y, this.width, this.height)
         break
     }
   }
@@ -75,15 +100,25 @@ class Obstacle {
 /*****************************************************/
 /****************** THE INSTANCES *******************/
 /***************************************************/
-const runner = new Runner(121, 172)
+const player = new Runner(121, 172)
 // Generating random obstacles
+
 function getRandomObstacles() {
-  if (frames % 200 === 0) {
+  const minLevel = 350
+  const maxLevel = 250
+  let randomLevel = 200
+  if (score > 50) {
+    randomLevel = Math.floor(Math.random() * maxLevel + minLevel)
+  }
+
+  const randomImage = Math.floor(Math.random() * 3)
+
+  if (frames % randomLevel === 0) {
     // All obstacles will have the same width and height
     const width = 75
     const height = 75
     // Creating the obstacles as new instances
-    const obstacle = new Obstacle(canvas.height - height, width, height, 0)
+    const obstacle = new Obstacle(canvas.height - height, width, height, randomImage)
     obstacles.push(obstacle)
   }
 }
@@ -111,43 +146,97 @@ function clearWelcomeScreen() {
 }
 
 function update() {
-  frames++
   ctx.clearRect(0, 0, canvas.width, canvas.height)
-  runner.draw()
+  frames++
+  player.draw()
+  move()
   getRandomObstacles()
   drawObstacles()
   checkCollition()
+  drawScore()
 }
 
 function startGame() {
+  bgSong.volume = 0.125
   goFullScreen(document.querySelector('body'))
   clearWelcomeScreen()
-  interval = setInterval(update, 1000 / 60)
+  interval = setInterval(update, 1000 / 120)
+}
+
+function move() {
+  if (!player.grounded) {
+    player.y += player.velY
+    player.velY += gravity
+  }
+  if (player.y > canvas.height - player.height) {
+    player.grounded = true
+    player.jumping = false
+    player.y = canvas.height - player.height
+  }
+  if (keys[32]) {
+    if (!player.jumping) {
+      jumpSound.play()
+      player.velY = 0
+      player.grounded = false
+      player.jumping = true
+      player.velY += -player.jumpStrength * 2
+    }
+  }
 }
 
 function checkCollition() {
   obstacles.forEach(obstacle => {
-    if (runner.crashedWith(obstacle)) return gameOver()
+    if (player.crashedWith(obstacle)) return gameOver()
   })
+}
+
+function drawScore() {
+  if (frames % 50 === 0) {
+    score++
+  }
+  scoreBoard.innerHTML = `<p>${score}</p>`
 }
 
 function gameOver() {
   clearInterval(interval)
-  canvas.classList.add('loser')
+  bgSong.pause()
+  updateShareLinks()
+  showGameOverScreen()
+  canvasWrapper.classList.add('loser')
   gameOverSong.play()
+}
+
+function updateShareLinks() {
+  finalScore.innerText = score
+  whatsSharer.innerHTML = `<a href="https://wa.me/?text=Te%20reto%20a%20superar%20mi%20puntaje%3A%20%F0%9F%91%89%20${score}pts%0A%0Ahttps%3A%2F%2Fpostmodernidad.netlify.com%2F" rel="noopener, noreferrer" target="_blank" title="Share on Whatsapp"><i class="fa fa-whatsapp"></i></a>`
+  fbSharer.innerHTML = `<a href="https://www.facebook.com/sharer/sharer.php?u=https://postmodernidad.netlify.com/&t=Te%20reto%20a%20superar%20mi%20puntaje%3A%20%F0%9F%91%89%20${score}pts" rel="noopener, noreferrer" target="_blank" title="Share on Facebook"><i class="fa fa-facebook-f"></i></a>`
+  mailSharer.innerHTML = `<a href="mailto:?subject=Te%20reto%20a%20superar%20mi%20puntaje%3A%20%F0%9F%91%89%20${score}pts&body=Juega%20dando%20click%20aqu%C3%AD%3A%0A%0Ahttps%3A%2F%2Fpostmodernidad.netlify.com" rel="noopener, noreferrer" target="_blank" title="Mail your friend"><i class="fa fa-envelope"></i></a>`
+}
+
+function showGameOverScreen() {
+  gameOverScreen.style.display = 'block'
 }
 
 /**************************************/
 /************* LISTENERS *************/
 /************************************/
-document.addEventListener('keydown', e => {
-  if (frames > 0) {
-    switch (e.keyCode) {
-      case 32:
-        runner.jump()
-        break
-    }
-  } else {
-    startGame()
+window.onload = () => {
+  bgSong.volume = 0.0975
+  bgSong.play()
+}
+
+addEventListener('keydown', e => {
+  switch (e.keyCode) {
+    case 32: // Space Bar Keycode
+      frames === 0 ? startGame() : (keys[e.keyCode] = true)
+      break
   }
+})
+
+addEventListener('keyup', e => {
+  keys[e.keyCode] = false
+})
+
+restartButton.addEventListener('click', () => {
+  location.reload()
 })
